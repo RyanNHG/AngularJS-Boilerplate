7b0011ae9afee64d95c82627a39ca1efcafafe63
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2016-02-25 00:26:07 -0600

    Added cool color effect to navbar based on route

--- webapp/src/app/navbar/navbar.ctrl.js
+++ webapp/src/app/navbar/navbar.ctrl.js
@@ -3,6 +3,8 @@ function($scope, $location){
 
     var self = this;
 
+    this.enableColorChange = true;
+
     this.navLinks = {
         left: [
             {
@@ -35,11 +37,26 @@ function($scope, $location){
     // Update active link on route change
     $scope.$on('$routeChangeSuccess', function(){
         self.path = $location.path();
+        self.getNavColor();
     });
 
     //  Go to another page
     this.go = function(path) {
         $location.path(path);
-    }
+    };
+
+    //  Do cool stuff with the nav color
+    this.getNavColor = function() {
+        var path = this.path;
+
+        if(this.enableColorChange)
+            this.navColor = (path == '/profile') ? 'red' :
+                (path == '/dashboard') ? 'green' :
+                (path == '/settings') ? 'purple': 
+                (path == '/') ? 'blue' : 'gray';
+        else this.navColor = 'blue';
+    };
+
+    this.getNavColor();
 
 }];
