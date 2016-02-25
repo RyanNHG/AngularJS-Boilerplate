9cbf6b573ea54321c6b924f2a0a983b38ad6509a
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2016-02-24 18:25:56 -0600

    Complete bear api;

--- api/bears/api.js
+++ api/bears/api.js
@@ -28,7 +28,6 @@ module.exports = function(router) {
             }
             else
             {
-
                 bear.save(function(err) {
                     
                     if(err) 
@@ -44,6 +43,7 @@ module.exports = function(router) {
 
     router.route('/bears/:bear_id')
 
+        //  Get a bear by ID
         .get(function(req,res){
 
             Bear.findById(req.params.bear_id, function(err, bear) {
@@ -53,6 +53,51 @@ module.exports = function(router) {
                 res.json(bear);
 
             });
+
+        })
+
+        //  Update a bear by ID
+        .put(function(req, res) {
+
+            if(req.body.name == null)
+            {
+                res.json({ message: 'Bear name not provided!' });
+            }
+            else
+            {
+                Bear.findById(req.params.bear_id, function(err, bear){
+
+                    if(err) res.send(err);
+
+                    bear.name = req.body.name;
+
+                    bear.save(function(err) {
+                        
+                        if(err) 
+                            res.send(err);
+
+                        res.json({ message: bear.name + ' updated!' });       
+                    });
+
+                });
+            }
+
+        })
+
+        .delete(function(req, res){
+
+            Bear.remove(
+                {
+                    _id: req.params.bear_id
+                },
+                function(err, bear) {
+                    
+                    if(err)  
+                        res.send(err);
+
+                    res.json({message: 'Bear deleted!'});
+                }
+            )
 
         })
 
